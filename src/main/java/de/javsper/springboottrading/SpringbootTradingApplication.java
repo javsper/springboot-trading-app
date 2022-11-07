7f8563611efd8b8a1baa611d461226a5e49d6909
javsper <goldlion1219@gmail.com>
2022-11-07 20:51:20 +0200

    initial commit already added IBKR dependency

---
+++ src/main/java/de/javsper/springboottrading/SpringbootTradingApplication.java
@@ -0,0 +1,13 @@
+package de.javsper.springboottrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTradingApplication.class, args);
+	}
+
+}
