80ee0b6d2d91b122c6142babc14eae05498126fc
javsper <goldlion1219@gmail.com>
2022-11-23 10:08:39 +0100

    resolved Datasource issues in multi Module Project

--- springboot-trading-web/src/main/java/de/javsper/springboottradingweb/SpringbootTradingApplication.java
+++ springboot-trading-web/src/main/java/de/javsper/springboottradingweb/SpringbootTradingApplication.java
@@ -2,8 +2,12 @@ package de.javsper.springboottradingweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "de.javsper.springboottradingdata.dao")
+@EntityScan(basePackages = "de.javsper.springboottradingdata.ds")
 public class SpringbootTradingApplication {
 
 	public static void main(String[] args) {
