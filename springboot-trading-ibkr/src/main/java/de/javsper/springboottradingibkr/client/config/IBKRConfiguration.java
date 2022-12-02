bd15fec17da1d22fd049c0dda49933753ff8a639
javsper <goldlion1219@gmail.com>
2022-12-02 12:50:48 +0100

    changed EClientSocket Injection to setter Injection to work around circular dependency from ibkr api

--- springboot-trading-ibkr/src/main/java/de/javsper/springboottradingibkr/client/config/IBKRConfiguration.java
+++ springboot-trading-ibkr/src/main/java/de/javsper/springboottradingibkr/client/config/IBKRConfiguration.java
@@ -1,6 +1,8 @@
 package de.javsper.springboottradingibkr.client.config;
 
+import com.ib.client.EClientSocket;
 import com.ib.client.EJavaSignal;
+import de.javsper.springboottradingibkr.client.IBKRConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,11 @@ public class IBKRConfiguration {
     @Bean
     public EJavaSignal eJavaSignal(){
         return new EJavaSignal();
+    }
+
+    @Bean
+    public EClientSocket eClientSocket(EJavaSignal eJavaSignal, IBKRConnection ibkrConnection) {
+        return new EClientSocket(ibkrConnection,eJavaSignal);
     }
 
 }
