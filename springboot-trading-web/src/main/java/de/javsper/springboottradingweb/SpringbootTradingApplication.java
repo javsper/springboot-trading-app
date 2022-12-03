775f5178342746ad42b6611b1ebdc8971b4d7b03
javsper <goldlion1219@gmail.com>
2022-12-03 14:51:54 +0100

    added connect Functionality and removed a circular dependency on startup

--- springboot-trading-web/src/main/java/de/javsper/springboottradingweb/SpringbootTradingApplication.java
+++ springboot-trading-web/src/main/java/de/javsper/springboottradingweb/SpringbootTradingApplication.java
@@ -3,11 +3,13 @@ package de.javsper.springboottradingweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "de.javsper.springboottradingdata.dao")
-@EntityScan(basePackages = "de.javsper.springboottradingdata.ds")
+@EnableJpaRepositories(basePackages = "de.javsper")
+@EntityScan(basePackages = "de.javsper")
+@ComponentScan(basePackages = "de.javsper")
 public class SpringbootTradingApplication {
 
 	public static void main(String[] args) {
