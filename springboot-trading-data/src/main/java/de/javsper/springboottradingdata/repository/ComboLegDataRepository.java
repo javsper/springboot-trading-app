c424212e93d7a0f14a8a235cc0e565586adab9b0
javsper <goldlion1219@gmail.com>
2022-12-15 22:13:08 +0100

    added logic to build Iron Condor Contract, have to work on the Entity Relations

---
+++ springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/ComboLegDataRepository.java
@@ -0,0 +1,7 @@
+package de.javsper.springboottradingdata.repository;
+
+import de.javsper.springboottradingdata.model.ComboLegData;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ComboLegDataRepository  extends CrudRepository<ComboLegData, Integer> {
+}
---
+++ springboot-trading-ibkr/src/main/java/de/javsper/springboottradingibkr/client/strategybuilder/type/Leg.java
@@ -0,0 +1,8 @@
+package de.javsper.springboottradingibkr.client.strategybuilder.type;
+
+public enum Leg {
+    BUY_PUT,
+    BUY_CALL,
+    SELL_PUT,
+    SELL_CALL
+}
