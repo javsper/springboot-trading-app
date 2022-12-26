0eddbd3b6a7c4111f99d271192e80064319f3357
javsper <goldlion1219@gmail.com>
2022-12-26 14:38:52 +0100

    refactoring to make Return Values to Controllers from Database Optional

--- springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/message/ErrorMessageRepository.java
+++ springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/message/ErrorMessageRepository.java
@@ -3,5 +3,9 @@ package de.javsper.springboottradingdata.repository.message;
 import de.javsper.springboottradingdata.model.message.ErrorMessage;
 import de.javsper.springboottradingdata.repository.BaseRepository;
 
+import java.util.List;
+
 public interface ErrorMessageRepository extends BaseRepository<ErrorMessage> {
+
+    public List<ErrorMessage> findAllByErrorId(Integer errorId);
 }
--- springboot-trading-data/src/main/java/de/javsper/springboottradingdata/service/ApiResponseInEntityChecker.java
+++ springboot-trading-data/src/main/java/de/javsper/springboottradingdata/service/ApiResponseInEntityChecker.java
@@ -2,23 +2,27 @@ package de.javsper.springboottradingdata.service;
 
 import de.javsper.springboottradingdata.model.IBKRDataTypeEntity;
 import de.javsper.springboottradingdata.repository.IBKRDataTypeRepository;
+import de.javsper.springboottradingdata.repository.message.ErrorMessageRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ApiResponseInEntityChecker {
 
     private final RepositoryRefreshService repositoryRefreshService;
+    private final ErrorMessageRepository errorMessageRepository;
 
-    public ApiResponseInEntityChecker(RepositoryRefreshService repositoryRefreshService) {
+    public ApiResponseInEntityChecker(RepositoryRefreshService repositoryRefreshService, ErrorMessageRepository errorMessageRepository) {
         this.repositoryRefreshService = repositoryRefreshService;
+        this.errorMessageRepository = errorMessageRepository;
     }
 
-    public <T extends IBKRDataTypeEntity>T checkForApiResponseAndUpdate(IBKRDataTypeRepository<T> repository, Integer id) {
-        T entity;
+    public <T extends IBKRDataTypeEntity> Optional<T> checkForApiResponseAndUpdate(IBKRDataTypeRepository<T> repository, Integer id) {
         do {
             repositoryRefreshService.clearCacheAndWait(repository);
-            entity = repository.findById(id).orElseThrow();
-        }while(!entity.isTouchedByApi());
-            return entity;
-        }
+        } while (repository.findById(id).isEmpty() && errorMessageRepository.findAllByErrorId(id).isEmpty());
+        //TODO: If the error Id gets ambiguous for whatever reason: include check by timestamp older than x
+        return repository.findById(id);
+    }
 }
