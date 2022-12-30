4155e2b8a52031337c602aa2869c4874bfd2a0d3
javsper <goldlion1219@gmail.com>
2022-12-30 17:40:48 +0100

    changed all ticker messages to Kafka messages

--- springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/message/TickerMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package de.javsper.springboottradingdata.repository.message;
-
-import de.javsper.springboottradingdata.model.message.TickerMessage;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TickerMessageRepository extends CrudRepository<TickerMessage, Integer> {
-}
