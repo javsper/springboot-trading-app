e5fec28555846af7f16f6fd3417ede4b702bda11
javsper <goldlion1219@gmail.com>
2023-01-02 16:42:36 +0100

    added more functionality for historical Data plus fix for malformed time Strings

--- springboot-trading-data/src/main/java/de/javsper/springboottradingdata/service/OrderWriteToDBService.java
+++ springboot-trading-data/src/main/java/de/javsper/springboottradingdata/service/OrderWriteToDBService.java
@@ -5,7 +5,7 @@ import com.ib.client.Order;
 import com.ib.client.OrderStatus;
 import de.javsper.springboottradingdata.model.ContractData;
 import de.javsper.springboottradingdata.model.OrderData;
-import de.javsper.springboottradingdata.modelconverter.ContractDataDatabasseSynchronizer;
+import de.javsper.springboottradingdata.modelconverter.ContractDataDatabaseSynchronizer;
 import de.javsper.springboottradingdata.modelconverter.IBKROrderToOrderData;
 import de.javsper.springboottradingdata.repository.OrderDataRepository;
 import org.springframework.stereotype.Service;
@@ -16,16 +16,16 @@ import java.util.OptionalLong;
 public class OrderWriteToDBService {
     private final OrderDataRepository orderDataRepository;
     private final IBKROrderToOrderData ibkrOrderToOrderData;
-    private final ContractDataDatabasseSynchronizer contractDataDatabasseSynchronizer;
+    private final ContractDataDatabaseSynchronizer contractDataDatabaseSynchronizer;
 
-    public OrderWriteToDBService(OrderDataRepository orderDataRepository, IBKROrderToOrderData ibkrOrderToOrderData, ContractDataDatabasseSynchronizer contractDataDatabasseSynchronizer) {
+    public OrderWriteToDBService(OrderDataRepository orderDataRepository, IBKROrderToOrderData ibkrOrderToOrderData, ContractDataDatabaseSynchronizer contractDataDatabaseSynchronizer) {
         this.orderDataRepository = orderDataRepository;
         this.ibkrOrderToOrderData = ibkrOrderToOrderData;
-        this.contractDataDatabasseSynchronizer = contractDataDatabasseSynchronizer;
+        this.contractDataDatabaseSynchronizer = contractDataDatabaseSynchronizer;
     }
 
     public void saveOrUpdateFullOrderDataToDb(Order order, Contract contract, String orderStatus) {
-        ContractData contractData = contractDataDatabasseSynchronizer.findInDBOrConvertAndSaveOrUpdateIfIdIsProvided(OptionalLong.empty(), contract);
+        ContractData contractData = contractDataDatabaseSynchronizer.findInDBOrConvertAndSaveOrUpdateIfIdIsProvided(OptionalLong.empty(), contract);
         OrderData orderData = ibkrOrderToOrderData.convertOrder(order);
         orderData.setStatus(OrderStatus.get(orderStatus));
 
