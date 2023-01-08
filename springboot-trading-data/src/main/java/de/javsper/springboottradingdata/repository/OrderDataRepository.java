5470d5171f731637661c2ef9385d6b59d240d1b1
javsper <goldlion1219@gmail.com>
2023-01-08 13:00:33 +0100

    added TimeStamp to TwsMessage and save nextValidOrderId correctly

--- springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/OrderDataRepository.java
+++ springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/OrderDataRepository.java
@@ -2,5 +2,9 @@ package de.javsper.springboottradingdata.repository;
 
 import de.javsper.springboottradingdata.model.OrderData;
 
+import java.util.Optional;
+
 public interface OrderDataRepository  extends IBKRDataTypeRepository<OrderData> {
+
+    Optional<OrderData> findTopByOrderByIdDesc();
 }
