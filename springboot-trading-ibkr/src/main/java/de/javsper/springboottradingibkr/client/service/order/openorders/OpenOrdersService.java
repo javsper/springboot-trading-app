1d6e90860f71429346ef9140d635a66455f67f6e
javsper <goldlion1219@gmail.com>
2023-01-08 12:24:58 +0100

    Fix OpenOrders

--- springboot-trading-ibkr/src/main/java/de/javsper/springboottradingibkr/client/service/order/openorders/OpenOrdersService.java
+++ springboot-trading-ibkr/src/main/java/de/javsper/springboottradingibkr/client/service/order/openorders/OpenOrdersService.java
@@ -1,6 +1,7 @@
 package de.javsper.springboottradingibkr.client.service.order.openorders;
 
 import de.javsper.springboottradingdata.model.OrderData;
+import de.javsper.springboottradingdata.service.apiresponsecheck.noinput.OpenOrdersApiResponseChecker;
 import de.javsper.springboottradingibkr.client.service.ApiCallerWithoutParameter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -13,14 +14,15 @@ public class OpenOrdersService {
 
 
     private final ApiCallerWithoutParameter<OrderData> openOrderApiCaller;
+    private final OpenOrdersApiResponseChecker openOrdersApiResponseChecker;
 
-    public OpenOrdersService(@Qualifier("OpenOrdersApiCaller") ApiCallerWithoutParameter<OrderData> openOrderApiCaller) {
+    public OpenOrdersService(@Qualifier("OpenOrdersApiCaller") ApiCallerWithoutParameter<OrderData> openOrderApiCaller, OpenOrdersApiResponseChecker openOrdersApiResponseChecker) {
         this.openOrderApiCaller = openOrderApiCaller;
+        this.openOrdersApiResponseChecker = openOrdersApiResponseChecker;
     }
 
     public List<OrderData> getOpenOrders(){
         openOrderApiCaller.callApi();
-        //TODO here I need to build something with Kafka to get the Value!!
-        return new ArrayList<>();
+        return openOrdersApiResponseChecker.checkForApiResponseAndUpdate();
     }
 }
