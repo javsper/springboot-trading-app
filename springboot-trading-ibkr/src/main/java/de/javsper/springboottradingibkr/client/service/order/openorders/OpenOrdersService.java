63afb2a760ebaebf46fc3d6d790da805dcfb75a1
javsper <goldlion1219@gmail.com>
2023-01-30 15:54:22 +0100

    Change the way Open Orders are displayed, with Subscription and immediate Delete after cancel without checking in backend

--- springboot-trading-ibkr/src/main/java/de/javsper/springboottradingibkr/client/service/order/openorders/OpenOrdersService.java
+++ springboot-trading-ibkr/src/main/java/de/javsper/springboottradingibkr/client/service/order/openorders/OpenOrdersService.java
@@ -1,6 +1,7 @@
 package de.javsper.springboottradingibkr.client.service.order.openorders;
 
 import de.javsper.springboottradingdata.model.entity.OrderData;
+import de.javsper.springboottradingdata.repository.OrderDataRepository;
 import de.javsper.springboottradingdata.service.apiresponsecheck.noinput.OpenOrdersApiResponseChecker;
 import de.javsper.springboottradingibkr.client.service.ApiCallerWithoutParameter;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -14,13 +15,18 @@ public class OpenOrdersService {
 
     private final ApiCallerWithoutParameter<OrderData> openOrderApiCaller;
     private final OpenOrdersApiResponseChecker openOrdersApiResponseChecker;
+    private final OrderDataRepository orderDataRepository;
 
-    public OpenOrdersService(@Qualifier("OpenOrdersApiCaller") ApiCallerWithoutParameter<OrderData> openOrderApiCaller, OpenOrdersApiResponseChecker openOrdersApiResponseChecker) {
+    public OpenOrdersService(@Qualifier("OpenOrdersApiCaller") ApiCallerWithoutParameter<OrderData> openOrderApiCaller, OpenOrdersApiResponseChecker openOrdersApiResponseChecker, OrderDataRepository orderDataRepository) {
         this.openOrderApiCaller = openOrderApiCaller;
         this.openOrdersApiResponseChecker = openOrdersApiResponseChecker;
+        this.orderDataRepository = orderDataRepository;
     }
 
     public List<OrderData> getOpenOrders(){
+        orderDataRepository.deleteAll();
+        //clear any Lag with following line maybe. TODO: clean this code up
+//        openOrdersApiResponseChecker.checkForApiResponseAndUpdate();
         openOrderApiCaller.callApi();
         return openOrdersApiResponseChecker.checkForApiResponseAndUpdate();
     }
