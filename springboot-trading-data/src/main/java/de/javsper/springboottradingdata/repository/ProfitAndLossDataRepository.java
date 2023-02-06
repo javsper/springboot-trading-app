4cbafb8cf64c345f20b934d285d0f0854e5eca9b
javsper <goldlion1219@gmail.com>
2023-02-06 10:35:54 +0100

    Add ProfitAndLossData Entity, Repository and Add Id for Account in Properties

---
+++ springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/ProfitAndLossDataRepository.java
@@ -0,0 +1,6 @@
+package de.javsper.springboottradingdata.repository;
+
+import de.javsper.springboottradingdata.model.entity.ProfitAndLossData;
+
+public interface ProfitAndLossDataRepository extends IBKRDataTypeRepository<ProfitAndLossData>{
+}
