2751962b48ae6b3405d10da12fa5887025a2a02c
javsper <goldlion1219@gmail.com>
2023-02-16 13:56:23 +0100

    Refactoring after changing all Rest Calls to Stomp Websocket Responses

--- springboot-trading-ibkr/src/main/java/de/javsper/springboottradingibkr/client/service/order/openorders/OpenOrdersService.java
+++ springboot-trading-ibkr/src/main/java/de/javsper/springboottradingibkr/client/service/order/openorders/OpenOrdersService.java
@@ -1,33 +1,21 @@
 package de.javsper.springboottradingibkr.client.service.order.openorders;
 
-import de.javsper.springboottradingdata.model.entity.OrderData;
-import de.javsper.springboottradingdata.repository.OrderDataRepository;
-import de.javsper.springboottradingdata.service.apiresponsecheck.noinput.OpenOrdersApiResponseChecker;
 import de.javsper.springboottradingibkr.client.service.ApiCallerWithoutParameter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class OpenOrdersService {
 
 
     private final ApiCallerWithoutParameter openOrderApiCaller;
-    private final OpenOrdersApiResponseChecker openOrdersApiResponseChecker;
-    private final OrderDataRepository orderDataRepository;
 
-    public OpenOrdersService(@Qualifier("OpenOrdersApiCaller") ApiCallerWithoutParameter openOrderApiCaller, OpenOrdersApiResponseChecker openOrdersApiResponseChecker, OrderDataRepository orderDataRepository) {
+    public OpenOrdersService(@Qualifier("OpenOrdersApiCaller") ApiCallerWithoutParameter openOrderApiCaller) {
         this.openOrderApiCaller = openOrderApiCaller;
-        this.openOrdersApiResponseChecker = openOrdersApiResponseChecker;
-        this.orderDataRepository = orderDataRepository;
+
     }
 
-    public List<OrderData> getOpenOrders(){
-        orderDataRepository.deleteAll();
-        //clear any Lag with following line maybe. TODO: clean this code up
-//        openOrdersApiResponseChecker.checkForApiResponseAndUpdate();
+    public void requestOpenOrders(){
         openOrderApiCaller.callApi();
-        return openOrdersApiResponseChecker.checkForApiResponseAndUpdate();
     }
 }
