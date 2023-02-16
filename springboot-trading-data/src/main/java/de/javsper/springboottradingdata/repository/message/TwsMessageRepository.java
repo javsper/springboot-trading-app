cc25040483ecf840e7d4e6a9f21a1f82a0b30cd3
javsper <goldlion1219@gmail.com>
2023-02-16 19:30:55 +0100

    remove TWS Message repository and dependencies

--- springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/message/TwsMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package de.javsper.springboottradingdata.repository.message;
-
-import de.javsper.springboottradingdata.model.entity.message.TwsMessage;
-import org.springframework.data.repository.CrudRepository;
-
-public interface  TwsMessageRepository extends CrudRepository<TwsMessage, Integer> {
-}
