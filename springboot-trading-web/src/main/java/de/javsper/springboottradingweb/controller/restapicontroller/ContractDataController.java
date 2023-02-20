f89f049c8d0cefd435e54e45ae65bc07e22f6487
javsper <goldlion1219@gmail.com>
2023-02-20 15:17:41 +0100

    Changes preparing marketData for Combos

--- springboot-trading-web/src/main/java/de/javsper/springboottradingweb/controller/restapicontroller/ContractDataController.java
+++ springboot-trading-web/src/main/java/de/javsper/springboottradingweb/controller/restapicontroller/ContractDataController.java
@@ -1,15 +1,15 @@
 package de.javsper.springboottradingweb.controller.restapicontroller;
 
+import de.javsper.springboottradingdata.model.data.StrategyContractData;
 import de.javsper.springboottradingdata.model.data.entity.ContractData;
 import de.javsper.springboottradingdata.repository.ContractDataRepository;
-import de.javsper.springboottradingdata.service.LegMapService;
 import de.javsper.springboottradingibkr.client.service.contract.UniqueContractDataProvider;
 import de.javsper.springboottradingibkr.client.strategybuilder.StrategyBuilderService;
 import de.javsper.springboottradingweb.service.ResponseMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/contract")
@@ -18,50 +18,33 @@ public class ContractDataController {
     private final ContractDataRepository contractDataRepository;
     private final UniqueContractDataProvider uniqueContractDataProvider;
     private final StrategyBuilderService strategyBuilderService;
-    private final LegMapService legMapService;
     private final ResponseMapper responseMapper;
 
     public ContractDataController(ContractDataRepository contractDataRepository,
                                   UniqueContractDataProvider uniqueContractDataProvider,
-                                  StrategyBuilderService strategyBuilderService, LegMapService legMapService, ResponseMapper responseMapper) {
+                                  StrategyBuilderService strategyBuilderService,
+                                  ResponseMapper responseMapper) {
         this.contractDataRepository = contractDataRepository;
         this.uniqueContractDataProvider = uniqueContractDataProvider;
         this.strategyBuilderService = strategyBuilderService;
-        this.legMapService = legMapService;
         this.responseMapper = responseMapper;
     }
 
-    @PutMapping("/combo")
-    public ResponseEntity<ContractData> ComboLegContractData(@RequestBody ContractData contractData,
-                                                             @RequestParam(defaultValue = "0", name = "buyPutStrike") double buyPutStrike,
-                                                             @RequestParam(defaultValue = "0", name = "sellPutStrike") double sellPutStrike,
-                                                             @RequestParam(defaultValue = "0", name = "buyCallStrike") double buyCallStrike,
-                                                             @RequestParam(defaultValue = "0", name = "sellCallStrike") double sellCallStrike,
-                                                             @RequestParam(defaultValue = "0", name = "buyPutStrikeTwo") double buyPutStrikeTwo,
-                                                             @RequestParam(defaultValue = "0", name = "sellPutStrikeTwo") double sellPutStrikeTwo,
-                                                             @RequestParam(defaultValue = "0", name = "buyCallStrikeTwo") double buyCallStrikeTwo,
-                                                             @RequestParam(defaultValue = "0", name = "sellCallStrikeTwo") double sellCallStrikeTwo) {
+    @PostMapping("/combo")
+    public ResponseEntity<ContractData> ComboLegContractData(@RequestBody StrategyContractData strategyContractData) {
 
-        //TODO will have to implement solution to get combos like in Orders Controller
-
-
-
-        Optional<ContractData> savedContract = strategyBuilderService.getComboLegContractData(contractData,  legMapService.mapLegs(buyPutStrike,
-                sellPutStrike,
-                buyCallStrike,
-                sellCallStrike,
-                buyPutStrikeTwo,
-                sellPutStrikeTwo,
-                buyCallStrikeTwo,
-                sellCallStrikeTwo));
+        Optional<ContractData> savedContract =
+                strategyBuilderService.getComboLegContractData(strategyContractData.getContractData(),
+                        strategyContractData.getStrategyLegs());
 
         return responseMapper.mapResponse(savedContract);
     }
 
 
-    @PutMapping()
+    @PostMapping("/single")
     public ResponseEntity<ContractData> saveContractData(@RequestBody ContractData contractData) {
-        Optional<ContractData> savedContract = uniqueContractDataProvider.getExistingContractDataOrCallApi(contractData);
+        Optional<ContractData> savedContract = uniqueContractDataProvider.getExistingContractDataOrCallApi(
+                contractData);
         return responseMapper.mapResponse(savedContract);
     }
 
