074cf8b3683fc1f9cf996ea9c1f28501437dd9fa
javsper <goldlion1219@gmail.com>
2023-03-03 09:41:41 +0100

    Add Method to retrieve Contract Data by contract Id

--- springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/ContractDataRepository.java
+++ springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/ContractDataRepository.java
@@ -1,7 +1,6 @@
 package de.javsper.springboottradingdata.repository;
 
 import com.ib.client.Types;
-import de.javsper.springboottradingdata.model.data.entity.ComboLegData;
 import de.javsper.springboottradingdata.model.data.entity.ContractData;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,7 +12,6 @@ public interface ContractDataRepository  extends IBKRDataTypeRepository<Contract
 
     List<ContractData> findAllByContractId(Integer id);
     Optional<ContractData> findFirstByContractId(Integer id);
-    List<ContractData> findByComboLegsContains(ComboLegData comboLegData);
     List<ContractData> findByComboLegsDescriptionContains(String contractId);
 
     Optional<ContractData> findFirstBySymbolAndSecurityTypeAndCurrency(String symbol, Types.SecType secType,
