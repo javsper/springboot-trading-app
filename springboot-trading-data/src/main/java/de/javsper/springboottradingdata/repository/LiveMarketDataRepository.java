5c13f4db311877e2b6832a4891cdbf44a20785ac
javsper <goldlion1219@gmail.com>
2023-08-26 16:39:08 +0200

    Add MarketData Entity

---
+++ springboot-trading-data/src/main/java/de/javsper/springboottradingdata/repository/LiveMarketDataRepository.java
@@ -0,0 +1,7 @@
+package de.javsper.springboottradingdata.repository;
+
+import de.javsper.springboottradingdata.model.data.entity.LiveMarketData;
+
+
+public interface LiveMarketDataRepository  extends IBKRDataTypeRepository<LiveMarketData> {
+}
