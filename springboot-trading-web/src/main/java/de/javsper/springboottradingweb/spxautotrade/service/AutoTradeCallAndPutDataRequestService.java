a4383ebc8aa1c6e0ef8a71d1a589aef1a896df8e
javsper <goldlion1219@gmail.com>
2023-10-14 10:09:30 +0200

    Renaming and adding fields

--- springboot-trading-web/src/main/java/de/javsper/springboottradingweb/spxautotrade/service/AutoTradeCallAndPutDataRequestService.java
+++ springboot-trading-web/src/main/java/de/javsper/springboottradingweb/spxautotrade/service/AutoTradeCallAndPutDataRequestService.java
@@ -3,6 +3,7 @@ package de.javsper.springboottradingweb.spxautotrade.service;
 import com.ib.client.Types;
 import de.javsper.springboottradingdata.config.TradingConstants;
 import de.javsper.springboottradingdata.model.data.entity.ContractData;
+import de.javsper.springboottradingdata.model.subtype.Symbol;
 import de.javsper.springboottradingdata.optionstradingservice.LastTradeDateBuilder;
 import de.javsper.springboottradingibkr.client.service.marketdata.StartMarketDataService;
 import java.math.BigDecimal;
@@ -24,11 +25,11 @@ public class AutoTradeCallAndPutDataRequestService {
             ContractData.ContractDataBuilder builder =
                     ContractData.builder().securityType(Types
                                     .SecType.OPT)
-                            .symbol(TradingConstants.SPX)
+                            .symbol(Symbol.SPX.name())
                             .exchange(TradingConstants.CBOE)
                             .currency(TradingConstants.USD)
-                            .lastTradeDate(lastTradeDateBuilder.getDateStringFromToday())
-                            .tradingClass(TradingConstants.SPXW);
+                            .lastTradeDate(lastTradeDateBuilder.getDateStringFromTomorrow())
+                            .tradingClass(Symbol.SPXW.name());
             int callPrice = strike + strikediff;
             ContractData call = builder.strike(BigDecimal.valueOf(callPrice)).right(Types.Right.Call).build();
             callMarketData(call);
