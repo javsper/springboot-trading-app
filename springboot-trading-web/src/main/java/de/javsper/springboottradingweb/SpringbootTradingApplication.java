99f84f8b971cf1af76549f3d4f61e71295c0fbf0
javsper <goldlion1219@gmail.com>
2023-11-06 09:12:24 +0200

    Changed Connection on develop to Live until paper account live data works. And added test code tgo SpringbootTradingApplication // Needs to be reverted

--- springboot-trading-web/src/main/java/de/javsper/springboottradingweb/SpringbootTradingApplication.java
+++ springboot-trading-web/src/main/java/de/javsper/springboottradingweb/SpringbootTradingApplication.java
@@ -1,6 +1,7 @@
 package de.javsper.springboottradingweb;
 
 import de.javsper.springboottradingdata.config.PropertiesConfig;
+import de.javsper.springboottradingweb.spxautotrade.scheduler.LiveMarketDataAutoTradeStarterScheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -26,6 +27,10 @@ public class SpringbootTradingApplication {
 		PropertiesConfig propertiesConfig = context.getBean(PropertiesConfig.class);
 		ConnectionInitiator connection = context.getBean(ConnectionInitiator.class);
 		connection.connect(propertiesConfig.getTradingPort());
+
+		//Test only
+		LiveMarketDataAutoTradeStarterScheduler liveMarketDataAutoTradeStarterScheduler = context.getBean(LiveMarketDataAutoTradeStarterScheduler.class);
+		liveMarketDataAutoTradeStarterScheduler.getOptionDataForDayTradeStrategy();
 	}
 
 }
